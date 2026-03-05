3420a33d0ae958eab60056612393e8582039649f
jspaglinawan <jspaglinawan.student@asiancollege.edu.ph>
2026-03-05 08:39:42 +0800

    Delete todolist directory

--- todolist/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "todo_db");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
--- todolist/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $sql = "DELETE FROM tasks WHERE id=$id";
-    $conn->query($sql);
-}
-?>
